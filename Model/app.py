ad2db308056cdcec8a2c71a5b04e511f33174cb3
Chaitanya Kumar Sinha <71923855+AltNeo@users.noreply.github.com>
2023-11-20 01:30:46 +0530

    Fixed the app.py file (background now visible)

--- Model/app.py
+++ Model/app.py
@@ -9,19 +9,19 @@ app = Flask(__name__)
 model = pickle.load(open(r'xg.model.pkl', 'rb'))
 @app.route('/')
 def home():
-    return render_template('templates\index.html') #rendering the home page?
+    return render_template('index.html') #rendering the home page?
 
 @app.route('/predict',methods=["POST","GET"]) # route to show the predictions in a web UI
 def predict():
     # reading the inputs given by the user
     input_feature=[float(x) for x in request.form.values()]
     features_values=[np.array(input_feature)]
-    names = [['playerID','Sex','Equipment','Age','BodyweightKg','BestSquatKg', 'BestBenchKg']]
-    data = pd.DataFrame(features_values, column=names)
+    names = [['playerId','Sex','Equipment','Age','BodyweightKg','BestSquatKg', 'BestBenchKg']]
+    data = pd.DataFrame(features_values, columns=names)
     prediction = model.predict(data)
     print(prediction)
     text = "Estimated Deadlift for the builder is: "
-    return render_template("predict.html", prediction_text = text + str(prediction))
+    return render_template("index.html", prediction_text = text + str(prediction))
 
 if __name__ == '__main__':
     app.run(debug=True)
